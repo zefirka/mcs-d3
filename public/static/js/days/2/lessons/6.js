9a19d7f4014fead630f6ea704db8f9c805e3ba7b
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2015-10-02 16:09:57 +0300

    Added D3 example

--- public/static/js/days/2/lessons/6.js
+++ public/static/js/days/2/lessons/6.js
@@ -1,16 +1,20 @@
 var BIG_DATA = [16, 23, 42, 100, 20, 77, 22, 2];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(BIG_DATA)])
-    .range([0, 420]);
+// var x = d3.scale.linear()
+//     .domain([0, d3.max(BIG_DATA)])
+//     .range([0, 420]);
 
-d3.select(".chart")
-  .selectAll("div")
+d3.select('.container')
+  .selectAll('div')
     .data(BIG_DATA)
     .enter()
-    .append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .style('background-color', 'red')
-    .style('margin-bottom', '2px')
-    .text(function(d) { return d; });
+    .append('div')
+    .attr('class', 'bar')
+    .style('width', function(d) { 
+        return d + 'px';
+        //return x(d) + 'px'; 
+    })
+    .text(function(d) { 
+        return d; 
+    });
 
