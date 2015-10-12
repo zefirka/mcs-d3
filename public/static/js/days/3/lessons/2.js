31455a1a9f804d5d335420679275ed1d709b9cfb
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2015-10-12 16:09:32 +0300

    Added bar-chart with words

--- public/static/js/days/3/lessons/2.js
+++ public/static/js/days/3/lessons/2.js
@@ -1,150 +1,226 @@
 var cache = (function(){
-	var store;
-	return {
-		cached: false,
-		get: function(){
-			return store;
-		},
-		set: function(x){
-			store = x;
+    var store;
+    return {
+        cached: false,
+        get: function(){
+            return store;
+        },
+        set: function(x){
+            store = x;
 
-			if (x === undefined){
-				this.cached = false;
-			} else {
-				this.cached = true;
-			}
-		}
-	}
+            if (x === undefined){
+                this.cached = false;
+            } else {
+                this.cached = true;
+            }
+        }
+    }
 })();
 
 var $limit = $('#limit');
+
 $limit.on('change', function(){
-	draw(null, Number(this.value));
+    draw(null, Number(this.value));
 });
 
 $.ajax({
-	method: 'GET',
-	url: 'story.txt',
-	dataType: 'text',
-	success: function(text){
-		draw(text, $limit.val());
-	},
-	error: function (error){
-		alert('error');
-	}
+    method: 'GET',
+    url: 'lovecraft.txt',
+    dataType: 'text',
+    success: function(text){
+        draw(text, $limit.val());
+    },
+    error: function (error){
+        alert('error');
+    }
 });
 
 function notInExceptions(word){
-	return exeptions.every(function(exeption){
-		return typeof exeption == 'string' ? word !== exeption : !exeption.test(word);
-	});
+    return exeptions.every(function(exeption){
+        return typeof exeption == 'string' ? word !== exeption : !exeption.test(word);
+    });
 }
 
 function parse(text, limit){
-	var table = [];
+    var table = [];
 
-	if(!cache.cached){
-		var words = text.replace(/[\.,\?\!\;\:\-«»\"\'\\(]/g, ' ').split(/\s/);
-		var dict = words.map(function(word){
-			return word.toLowerCase();
-		}).filter(function(word){
-			return word.length > 4 && notInExceptions(word);
-		}).map(function(word){
-			var match = equals.filter(function(matcher){
-				return matcher.reg.test(word);
-			});
+    if(!cache.cached){
+        var words = text.replace(/[\.,\?\!\;\:\-«»\"\'\\(]/g, ' ').split(/\s/);
+        var dict = words.map(function(word){
+            return word.toLowerCase();
+        }).filter(function(word){
+            return word.length > 4 && notInExceptions(word);
+        }).map(function(word){
+            var match = equals.filter(function(matcher){
+                return matcher.reg.test(word);
+            });
 
-			if(match.length){
-				word = match[0].to;
-			}
+            if(match.length){
+                word = match[0].to;
+            }
 
-			return word;
-		})
-		.reduce(function(sum, word){
-			if (sum[word]){
-				sum[word] += 1;
-			} else {
-				sum[word] = 1;
-			}
-			return sum;
-		}, {});
-		
-		for(var word in dict){
-			table.push({
-				word: word,
-				count: dict[word]
-			});
-		};
-		
-		table.sort(function(a, b){
-			return b.count - a.count;
-		});
+            return word;
+        })
+        .reduce(function(sum, word){
+            if (sum[word]){
+                sum[word] += 1;
+            } else {
+                sum[word] = 1;
+            }
+            return sum;
+        }, {});
+        
+        for(var word in dict){
+            table.push({
+                word: word,
+                count: dict[word]
+            });
+        };
+        
+        table.sort(function(a, b){
+            return b.count - a.count;
+        });
 
-		cache.set(table);
-	}else{
-		table = cache.get();
-	}
+        cache.set(table);
+    }else{
+        table = cache.get();
+    }
 
-	return table.slice(0, limit);
+    return table.slice(0, limit);
 }
 
 function prop(x){
-	return function(d){
-		return d[x];
-	};
+    return function(d){
+        return d[x];
+    };
 }
+
 
 var getCount = prop('count');
 var getWord = prop('word');
 
 var PADDING = 5;
 var BAR_WIDTH = 40;
-var MARGIN = PADDING + BAR_WIDTH;
-var CHART_HEIGHT = 500;
+//var MARGIN = PADDING + BAR_WIDTH;
 
-var y = d3.scale.linear().range([0, CHART_HEIGHT])
+var CHART = {
+    height: 500
+}
+
+var margin = {
+    left: 20,
+    top: 20,
+    right: 20,
+    bottom: 20, 
+    bar: 5
+}
+
+var y = d3.scale.linear().range([CHART.height, 0]);
+
+var chart = d3.select('.chart')
+            .attr('height', CHART.height)
 
 function draw(text, limit){
-	var words = parse(text, limit);
+    var words = parse(text, limit);
 
-	y.domain([
-		d3.min(words, getCount),
-		d3.max(words, getCount),
-	]);
+    CHART.width = words.length * BAR_WIDTH;
 
-	d3.select('.chart').html('');
+    chart = chart.attr('width', CHART.width);
 
-	var svg = d3.select('.chart')
-				.attr('width', words.length * MARGIN)
-				.attr('height', CHART_HEIGHT);
+    var x = d3.scale.ordinal()
+        .domain(words.map(getWord))
+        .rangeRoundBands([0, CHART.width], .1);
 
-	var bar = svg 	
-		.selectAll("g")
-    	.data(words)
-  		.enter().append("g")
-    	.attr("transform", function(d, i) { 
-    		return "translate(" + (i *  MARGIN) + ",0)"; 
-    	});
+    y.domain([
+        d3.min(words, getCount),
+        d3.max(words, getCount),
+    ]);
 
-    bar .append("rect")
-    	.attr('y', function(d) {
-    		return CHART_HEIGHT - y(d.count);
-    	})
-    	.attr("height", function(d){
-    		return y(d.count);
-    	})
-    	.attr("width", BAR_WIDTH)
+    chart.html('');
 
-    var rects = bar.selectAll('rect');
+    var bar = chart.selectAll("g")
+        .data(words)
+            .enter().append("g")
+        .attr("transform", function(d, i) { 
+            return "translate(" + i * BAR_WIDTH + ",0)"; 
+        });
     
-    rects.on('mouseover', function(data){
-    	var popup = d3	.select('body')
-    					.append('div')
-    					.attr('class', 'popup');
+    bar.append("rect")
+        .attr("y", function(d) { 
+            return y(d.count); 
+        })
+        .attr("height", function(d) { 
+            return CHART.height - y(d.count);
+        })
+        .attr("width", x.rangeBand());
 
-    	popup.append('span').text('Word:' + data.word);
-    	popup.append('span').text('Count:' + data.count);
-    }).on('mouseout', function(){
-    	d3.select('div.popup').remove();
-    })
+    bar.append("text")
+        .attr("x", x.rangeBand() / 2)
+        .attr("y", function(d) { 
+            return y(d.count) + 3; 
+        })
+        .attr("dy", ".75em")
+        .text(function(d) { 
+            return d.word + '(' + d.count + ')'; 
+        });
+
+    // bar.append("text")
+    //     .attr("x", x.rangeBand() / 2)
+    //     .attr("y", function(d) { 
+    //         return y(d.count) + 3; 
+    //     })
+    //     .attr("dy", ".75em")
+    //     .text(function(d) { 
+    //         return d.count; 
+    //     });
+
+    // var xAxis = d3.svg.axis()
+    //     .scale(x)
+    //     .orient("bottom");
+
+    // var yAxis = d3.svg.axis()
+    //     .scale(y)
+    //     .orient("left");
+
+    // svg.append("g")
+    //     .attr("class", "x axis")
+    //     .attr("transform", "translate(0," + CHART_HEIGHT + ")")
+    //     .call(xAxis);
+
+    // svg.append("g")
+    //     .attr("class", "x axis")
+    //     .call(yAxis);
+
+    // var bar = svg   
+    //     .selectAll("g")
+    //     .data(words)
+    //     .enter().append("g")
+    //     .attr("transform", function(d, i) { 
+    //         return "translate(" + (i *  MARGIN) + ",0)"; 
+    //     });
+
+    // bar .append("rect")
+    //     .attr('y', function(d) {
+    //         return CHART_HEIGHT - y(d.count);
+    //     })
+    //     .attr("height", function(d){
+    //         return y(d.count);
+    //     })
+    //     .attr("width", x.rangeBand())
+
+    // var rects = bar.selectAll('rect');
+
+
+
+        
+    // rects.on('mouseover', function(data){
+    //  var popup = d3  .select('body')
+    //                  .append('div')
+    //                  .attr('class', 'popup');
+
+    //  popup.append('span').text('Word:' + data.word);
+    //  popup.append('span').text('Count:' + data.count);
+    // }).on('mouseout', function(){
+    //  d3.select('div.popup').remove();
+    // })
 }
--- public/static/js/days/3/lessons/3.js
+++ public/static/js/days/3/lessons/3.js
@@ -1,145 +1,190 @@
 var cache = (function(){
-	var store;
-	return {
-		cached: false,
-		get: function(){
-			return store;
-		},
-		set: function(x){
-			store = x;
+    var store;
+    return {
+        cached: false,
+        get: function(){
+            return store;
+        },
+        set: function(x){
+            store = x;
 
-			if (x === undefined){
-				this.cached = false;
-			} else {
-				this.cached = true;
-			}
-		}
-	}
+            if (x === undefined){
+                this.cached = false;
+            } else {
+                this.cached = true;
+            }
+        }
+    }
 })();
 
 var $limit = $('#limit');
-var $origin = $('#origin');
+
+var exeptions = [
+    /^сво((и[хм])|(его)|(ей)|й)$/,
+    /^сказа((ть)|(л)|(ли))$/,
+    /^которы*/, 'после',
+    /^мож((ет)|(но))/, 'некоторые',
+    'когда', 'между', 'более', 'однако',
+    'только', 'нибудь', 'конечно', 
+    'перед', 'такой', 'среди', 'через', 'наших',
+    'всякий', 'теперь',
+    'чтобы', 'какой', 'тогда', 'долго',
+    'здесь', 'больше', 'будет',
+    'также', 'этого', 'пусть', 'вдруг'
+]
+
+var equals = [
+    { reg: /^самолет*/, to: 'самолет' },
+    { reg: /^врем[яе]*/, to: 'время' },
+    { reg: /^хребт*/, to: 'хребты' },
+    { reg: /^старц*/, to: 'старцы' },
+    { reg: /^земл*/, to: 'земля' }
+];
 
 $limit.on('change', function(){
-	draw(null, Number(this.value));
+    draw(null, Number(this.value));
 });
 
-$origin.on('keyup', function(){
-	cache.set();
-	draw(this.value, Number($limit.val()));
+$.ajax({
+    method: 'GET',
+    url: 'lovecraft.txt',
+    dataType: 'text',
+    success: function(text){
+        draw(text, $limit.val());
+    },
+    error: function (error){
+        alert('error');
+    }
 });
 
 function notInExceptions(word){
-	return exeptions.every(function(exeption){
-		return typeof exeption == 'string' ? word !== exeption : !exeption.test(word);
-	});
+    return exeptions.every(function(exeption){
+        return typeof exeption == 'string' ? word !== exeption : !exeption.test(word);
+    });
 }
 
 function parse(text, limit){
-	var table = [];
+    var table = [];
 
-	if(!cache.cached){
-		var words = text.replace(/[\.,\?\!\;\:\-«»\"\'\\(]/g, ' ').split(/\s/);
-		var dict = words.map(function(word){
-			return word.toLowerCase();
-		}).filter(function(word){
-			return word.length > 4 && notInExceptions(word);
-		}).map(function(word){
-			var match = equals.filter(function(matcher){
-				return matcher.reg.test(word);
-			});
+    if(!cache.cached){
+        var words = text.replace(/[\.,\?\!\;\:\-«»\"\'\\(]/g, '').split(/\s/);
+        var dict = words.map(function(word){
+            return word.toLowerCase();
+        }).filter(function(word){
+            return word.length > 4 && notInExceptions(word);
+        }).map(function(word){
+            var match = equals.filter(function(matcher){
+                return matcher.reg.test(word);
+            });
 
-			if(match.length){
-				word = match[0].to;
-			}
+            if(match.length){
+                word = match[0].to;
+            }
 
-			return word;
-		})
-		.reduce(function(sum, word){
-			if (sum[word]){
-				sum[word] += 1;
-			} else {
-				sum[word] = 1;
-			}
-			return sum;
-		}, {});
-		
-		for(var word in dict){
-			table.push({
-				word: word,
-				count: dict[word]
-			});
-		};
-		
-		table.sort(function(a, b){
-			return b.count - a.count;
-		});
+            return word;
+        })
+        .reduce(function(sum, word){
+            if (sum[word]){
+                sum[word] += 1;
+            } else {
+                sum[word] = 1;
+            }
+            return sum;
+        }, {});
+        
+        for(var word in dict){
+            table.push({
+                word: word,
+                count: dict[word]
+            });
+        };
+        
+        table.sort(function(a, b){
+            return b.count - a.count;
+        });
 
-		cache.set(table);
-	}else{
-		table = cache.get();
-	}
+        cache.set(table);
+    }else{
+        table = cache.get();
+    }
 
-	return table.slice(0, limit);
+    return table.slice(0, limit);
 }
 
 function prop(x){
-	return function(d){
-		return d[x];
-	};
+    return function(d){
+        return d[x];
+    };
 }
 
 var getCount = prop('count');
 var getWord = prop('word');
 
-var PADDING = 5;
-var BAR_WIDTH = 40;
-var MARGIN = PADDING + BAR_WIDTH;
-var CHART_HEIGHT = 500;
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    bar_width = 50,
+    width = 1024 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var y = d3.scale.linear().range([0, CHART_HEIGHT])
+var x = d3.scale.ordinal()
+    
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(5);
+
+var chart = d3.select("svg")
+    .attr("height", height + margin.top + margin.bottom);
+
+var svg = chart
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
 
 function draw(text, limit){
-	var words = parse(text, limit);
-
-	y.domain([
-		d3.min(words, getCount),
-		d3.max(words, getCount),
-	]);
-
-	d3.select('.chart').html('');
-
-	var svg = d3.select('.chart')
-				.attr('width', words.length * MARGIN)
-				.attr('height', CHART_HEIGHT);
-
-	var bar = svg 	
-		.selectAll("g")
-    	.data(words)
-  		.enter().append("g")
-    	.attr("transform", function(d, i) { 
-    		return "translate(" + (i *  MARGIN) + ",0)"; 
-    	});
-
-    bar .append("rect")
-    	.attr('y', function(d) {
-    		return CHART_HEIGHT - y(d.count);
-    	})
-    	.attr("height", function(d){
-    		return y(d.count);
-    	})
-    	.attr("width", BAR_WIDTH)
-
-    var rects = bar.selectAll('rect');
+    var data = parse(text, limit);
     
-    rects.on('mouseover', function(data){
-    	var popup = d3	.select('body')
-    					.append('div')
-    					.attr('class', 'popup');
+    chart.attr("width", bar_width * data.length + margin.left + margin.right);
+    svg.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    	popup.append('span').text('Word:' + data.word);
-    	popup.append('span').text('Count:' + data.count);
-    }).on('mouseout', function(){
-    	d3.select('div.popup').remove();
-    })
+    x = x.rangeRoundBands([0, bar_width * data.length - data.length], .1);
+    xAxis = xAxis.scale(x);
+
+    svg.html('');    
+    
+    x.domain(data.map(getWord));
+    y.domain([0, d3.max(data, getCount)]);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .attr('transform', "translate(-6, 0)")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .attr('font-size', "12px")
+      .style("text-anchor", "end")
+      .text("Количество сочетаний");
+
+    var bars = svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.word); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.count); })
+      .attr("height", function(d) { return height - y(d.count); });
 }
